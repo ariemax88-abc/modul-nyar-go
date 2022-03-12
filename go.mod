ffd374de8ed4abc119916f0bc6425328d993ccaa
Ari Bayu Prasetyo <aribprasetyo88@gmail.com>
2022-03-12 11:45:56 +0700

    Membuat modul baru -> created by: Ari Bayu Prasetyo

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ariemax88-abc/modul-nyar-go
+
+go 1.17
---
+++ say_HelloNew.go
@@ -0,0 +1,6 @@
+package main
+
+// jg lupa nama depan modul ubah menjadi huruf besar agar bisa diakses public
+func SayHelloNew() string {
+	return "Hello Modul sudah bisa diakses"
+}
